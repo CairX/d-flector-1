924ae2264f8d580fbfe238153a6c75fe453ae16d
Thomas Cairns <lazycairx@gmail.com>
2017-01-22 19:20:22 +0100

    Player object follows mouse position

---
+++ Assets/Scripts/MouseMotionFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMotionFollow : MonoBehaviour {
+
+    void Start () {
+        Cursor.visible = false;
+    }
+
+    void Update() {
+        Vector3 temp = Input.mousePosition;
+        Vector3 camera = Camera.main.ScreenToWorldPoint(temp);
+        camera.z = this.transform.position.z;
+        this.transform.position = camera;
+	}
+}
