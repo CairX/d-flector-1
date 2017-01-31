5ce8138c7a581d36c5fbf0ce8c63704b3223dec6
komster <komster97@gmail.com>
2017-01-31 12:26:15 +0100

    Added script for rotating the sheild

---
+++ Assets/Scrips/SheildRotating.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheildRotating : MonoBehaviour {
+
+    public int rotatinSpeed;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        this.transform.Rotate(Vector3.forward, rotatinSpeed, 0);
+    }
+}
