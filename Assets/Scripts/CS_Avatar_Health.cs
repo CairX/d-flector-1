65e6b5c0f68bb6b7c0693dd7ffa0be537d92ccf5
Thomas Cairns <lazycairx@gmail.com>
2017-02-08 09:34:26 +0100

    Display avatar health information through sprites

---
+++ Assets/Scripts/CS_Avatar_Health.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Avatar_Health : MonoBehaviour {
+
+    public SpriteRenderer invincibleSpriteRender;
+    public float invincibleTime;
+    private float timer;
+    private bool changeSprite;
+
+    public SpriteRenderer[] healthSpriteRenders;
+    private SpriteRenderer spriteRenderer;
+    private int index;
+
+	void Start ()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        index = 0;
+    }
+
+    void Update ()
+    {
+        timer -= Time.deltaTime;
+        if (changeSprite && timer <= 0)
+        {
+            UnityEditor.EditorUtility.CopySerialized(healthSpriteRenders[index], spriteRenderer);
+            changeSprite = false;
+            index++;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (timer <= 0 && index < healthSpriteRenders.Length - 1)
+        {
+            timer = invincibleTime;
+            UnityEditor.EditorUtility.CopySerialized(invincibleSpriteRender, spriteRenderer);
+            changeSprite = true;
+        } else if (timer <= 0 && index == healthSpriteRenders.Length - 1)
+        {
+            UnityEditor.EditorUtility.CopySerialized(healthSpriteRenders[index], spriteRenderer);
+            changeSprite = false;
+        }
+    }
+}
