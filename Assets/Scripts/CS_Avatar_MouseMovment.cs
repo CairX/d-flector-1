8e2f7653a93fbc1a9a6c2eb1fbd5a163f300a647
komster <komster97@gmail.com>
2017-02-10 12:57:34 +0100

    Collision with boundary

--- Assets/Scripts/CS_Avatar_MouseMovment.cs
+++ Assets/Scripts/CS_Avatar_MouseMovment.cs
@@ -7,6 +7,11 @@ public class CS_Avatar_MouseMovment : MonoBehaviour {
     private Vector2 mousePosition;
     private Vector2 mousePositionChange;
 
+    public float boundaryStartX;
+    public float boundaryStartY;
+    public float boundaryStopX;
+    public float boundaryStopY;
+
     void Start () {
         Cursor.visible = false;
     }
@@ -17,6 +22,11 @@ public class CS_Avatar_MouseMovment : MonoBehaviour {
         mousePositionChange.y = Input.GetAxis("Mouse Y");
         mousePosition.x = this.transform.position.x + mousePositionChange.x;
         mousePosition.y = this.transform.position.y + mousePositionChange.y;
-        this.transform.position = mousePosition;
+        Debug.Log(mousePosition.x);
+        if (mousePosition.x > boundaryStartX && mousePosition.x < boundaryStopX && mousePosition.y > boundaryStartY && mousePosition.y < boundaryStopY)
+        {
+            this.transform.position = mousePosition;
+        }
+
     }
 }
