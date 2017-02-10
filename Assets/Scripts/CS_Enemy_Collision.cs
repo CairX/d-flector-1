4ff85c3b124fe772c516987e90652cf6f282bf97
komster <komster97@gmail.com>
2017-02-10 15:19:42 +0100

    Collision between avatar and enemy

---
+++ Assets/Scripts/CS_Enemy_Collision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Enemy_Collision : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "Projectile")
+        {
+            Destroy(this.gameObject);
+        }
+        if (collision.gameObject.tag == "Shield")
+        {
+            Destroy(this.gameObject);
+        }
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/CS_Projectile_Collision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Projectile_Collision : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Enemy")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
