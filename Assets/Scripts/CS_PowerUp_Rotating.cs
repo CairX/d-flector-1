9ec491cd2adc68163d0382005aa1fbdad1094eee
komster <komster97@gmail.com>
2017-02-22 11:38:32 +0100

    Change the twin power up drop to look better.

---
+++ Assets/Scripts/CS_PowerUp_Rotating.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_PowerUp_Rotating : MonoBehaviour {
+
+    public float rotatinSpeed;
+
+	void Update () {
+        if (Time.timeScale <= 0) { return; }
+        this.transform.Rotate(Vector3.forward, rotatinSpeed, 0);
+    }
+}
