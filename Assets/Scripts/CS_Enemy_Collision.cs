3b715d91f501136c45e00300c18ba34290f75cf6
DanneDille <danne.dillchips@hotmail.com>
2017-02-22 10:36:43 +0100

    Enemys now drop slow motion powerUp

--- Assets/Scripts/CS_Enemy_Collision.cs
+++ Assets/Scripts/CS_Enemy_Collision.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CS_Enemy_Collision : MonoBehaviour {
 
     public GameObject twinShieldPowerUp;
+    public GameObject SlowMotionPowerUp;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -12,12 +13,18 @@ public class CS_Enemy_Collision : MonoBehaviour {
         if (cgo.tag == "Projectile" && cgo.GetComponent<CS_Projectile_Collision>().isAvatar())
         {
             Destroy(gameObject);
-            float randomValue = Random.Range(1.0f, 3.0f);
+            float randomValue = Random.Range(1.0f, 6.0f);
             if (randomValue >= 1.0f && randomValue <= 2.0f)
             {
                 twinShieldPowerUp.transform.position = transform.position;
                 Instantiate(twinShieldPowerUp);
             }
+            else if(randomValue >= 3.0f && randomValue <= 4.0f)
+            {
+                SlowMotionPowerUp.transform.position = transform.position;
+                Instantiate(SlowMotionPowerUp);
+            }
+
             CS_Notifications.Instance.Post(this, "EnemyDead");
         }
         if (cgo.tag == "Shield")
