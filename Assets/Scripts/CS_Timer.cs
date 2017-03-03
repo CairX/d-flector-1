9e65fd8dc12cf4e12c2fa9e18bcfebe2950d9562
Thomas Cairns <lazycairx@gmail.com>
2017-03-03 17:29:56 +0100

    Initialize timer script

---
+++ Assets/Scripts/CS_Timer.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CS_Timer : MonoBehaviour
+{
+    private Text text;
+    private float timer = 0.0f;
+    private bool running = false;
+
+    private void Awake()
+    {
+        CS_Notifications.Instance.Register(this, "OnPlayGame");
+        CS_Notifications.Instance.Register(this, "OnVictory");
+        CS_Notifications.Instance.Register(this, "OnGameOver");
+    }
+
+    private void OnDisable()
+    {
+        try
+        {
+            CS_Notifications.Instance.Unregister(this, "OnPlayGame");
+            CS_Notifications.Instance.Unregister(this, "OnVictory");
+            CS_Notifications.Instance.Unregister(this, "OnGameOver");
+        }
+        catch (System.NullReferenceException)
+        {
+            // Unity destroys objects in random order so there is no way
+            // to know if this is run before or after the
+            // notification center has been destroyed.
+        }
+    }
+
+    private void Start ()
+    {
+        text = GetComponent<Text>();
+    }
+
+    private void Update ()
+    {
+        if (!running) { return; }
+
+        timer += Time.deltaTime;
+
+        int minutes = Mathf.FloorToInt(timer / 60);
+        int seconds = Mathf.FloorToInt(timer % 60);
+        text.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    public void TimerStart()
+    {
+        timer = 0.0f;
+        running = true;
+    }
+
+    public void TimerPause()
+    {
+        running = false;
+    }
+
+    public void TimerResume()
+    {
+        running = true;
+    }
+
+    public void OnPlayGame()
+    {
+        TimerStart();
+    }
+
+    public void OnVictory()
+    {
+        TimerPause();
+    }
+
+    private void OnGameOver()
+    {
+        TimerPause();
+    }
+}
