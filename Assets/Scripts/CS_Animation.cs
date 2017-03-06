ef393da269dee3600acc8136bbd2ef903f842efa
DanneDille <danne.dillchips@hotmail.com>
2017-03-06 13:19:35 +0100

    workin on tutorial

---
+++ Assets/Scripts/CS_Animation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class CS_Animation : MonoBehaviour {
+
+    public Sprite[] tutorial;
+    private int frame;
+
+	// Use this for initialization
+	void Start () {
+        fruitSprites = Resources.LoadAll<Sprite>("fruits");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
