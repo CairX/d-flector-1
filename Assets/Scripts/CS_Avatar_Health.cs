6842fb033e25d2bf1bed9a5d05a3e0253ac650ab
Thomas Cairns <lazycairx@gmail.com>
2017-03-10 15:03:58 +0100

    When the avatar is invulnerable it doens't collide with anything

--- Assets/Scripts/CS_Avatar_Health.cs
+++ Assets/Scripts/CS_Avatar_Health.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class CS_Avatar_Health : MonoBehaviour {
 
-    public Sprite invincibleSprite;
-    public float invincibleTime;
-    private float timer;
+    public Sprite invulnerableSprite;
+    public float invulnerableTime;
+    private float invulnerableIimer;
+
+    private int invincibleLayer;
+    private int originalLayer;
+
     private bool changeSprite;
 
     public HealthPoint[] healthPoints;
@@ -34,24 +38,19 @@ public class CS_Avatar_Health : MonoBehaviour {
 
     private void Start ()
     {
+        invincibleLayer = LayerMask.NameToLayer("Void");
+        originalLayer = transform.parent.gameObject.layer;
+
         spriteRenderer = GetComponent<SpriteRenderer>();
         index = 0;
     }
 
     private void Update ()
     {
-        timer -= Time.deltaTime;
-        if (changeSprite && timer <= 0)
+        invulnerableIimer -= Time.deltaTime;
+        if (changeSprite && invulnerableIimer <= 0)
         {
-            spriteRenderer.sprite = healthPoints[index].avatar;
-            Color color = spriteRenderer.color;
-            color.a = 1.0f;
-            spriteRenderer.color = color;
-
-            healthPoints[index].hud.SetActive(false);
-
-            changeSprite = false;
-            index++;
+            BecomeVulnerable();
         }
     }
 
@@ -82,22 +81,54 @@ public class CS_Avatar_Health : MonoBehaviour {
     {
         CS_All_Audio.Instance.AvaterLoseHealth(healthPoints.Length - 1);
         CS_Medals.Instance.TookDamage();
-        if (timer <= 0 && index < healthPoints.Length - 1)
+        if (invulnerableIimer <= 0 && index < healthPoints.Length - 1)
         {
-            timer = invincibleTime;
-            spriteRenderer.sprite = invincibleSprite;
-            Color color = spriteRenderer.color;
-            color.a = 0.8f;
-            spriteRenderer.color = color;
-            changeSprite = true;
+            BecomeInvulnerable();
         }
-        else if (timer <= 0 && index == healthPoints.Length - 1)
+        else if (invulnerableIimer <= 0 && index == healthPoints.Length - 1)
         {
             healthPoints[index].hud.SetActive(false);
             spriteRenderer.sprite = healthPoints[index].avatar;
             changeSprite = false;
 
             CS_Notifications.Instance.Post(this, "OnGameOver");
+        }
+    }
+
+    private void BecomeInvulnerable()
+    {
+        invulnerableIimer = invulnerableTime;
+        spriteRenderer.sprite = invulnerableSprite;
+        Color color = spriteRenderer.color;
+        color.a = 0.8f;
+        spriteRenderer.color = color;
+
+        SetLayerRecursively(transform.parent.gameObject, invincibleLayer);
+
+        changeSprite = true;
+    }
+
+    private void BecomeVulnerable()
+    {
+        spriteRenderer.sprite = healthPoints[index].avatar;
+        Color color = spriteRenderer.color;
+        color.a = 1.0f;
+        spriteRenderer.color = color;
+
+        healthPoints[index].hud.SetActive(false);
+
+        SetLayerRecursively(transform.parent.gameObject, originalLayer);
+
+        changeSprite = false;
+        index++;
+    }
+
+    private static void SetLayerRecursively(GameObject parent, int layer)
+    {
+        parent.layer = layer;
+        foreach (Transform transform in parent.transform.GetComponentsInChildren<Transform>(true))
+        {
+            transform.gameObject.layer = layer;
         }
     }
 }
