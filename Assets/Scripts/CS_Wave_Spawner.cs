f20347de1523f9d1f6d682aa9ea61e936a2a8307
komster <komster97@gmail.com>
2017-03-13 14:52:21 +0100

    added the first level

--- Assets/Scripts/CS_Wave_Spawner.cs
+++ Assets/Scripts/CS_Wave_Spawner.cs
@@ -128,7 +128,7 @@ public class Wave
 public class WaveProp
 {
     public string name = "Enemy";
-    public enum EnemyType { BASIC, BOLAR, HOWLAL, SHOTGUN }
+    public enum EnemyType { BASIC, BOLAR, HOWLAL}
     public enum SpawnPos { A1, A2, A3, A4, A5, B1, B2, B3, B4, B5, C1, C2, C3, C4, C5 }
     public EnemyType enemy;
     [HideInInspector]
