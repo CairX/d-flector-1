2545c3f5748889e5032b72e958c5602a2293dbee
Thomas Cairns <lazycairx@gmail.com>
2017-03-15 10:33:36 +0100

    Start sound levels out equally

--- Assets/Scripts/CS_WorldManager.cs
+++ Assets/Scripts/CS_WorldManager.cs
@@ -7,7 +7,7 @@ public class CS_WorldManager : CS_Singleton<CS_WorldManager> {
 
     public CS_StateManager.State state = CS_StateManager.State.StartMenu;
     public int level = 0;
-    public float volumeMusic = 0.2f;
+    public float volumeMusic = 0.5f;
     public float volumeSoundEfeckt = 0.5f;
     public bool powerupExists = false;
 }
