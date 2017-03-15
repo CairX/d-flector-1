5b8b8031630e0f08535834116130c192c8c062cb
Thomas Cairns <lazycairx@gmail.com>
2017-03-15 09:43:01 +0100

    Destroy enemies when they are no longer visible by the camera
    Plus some refactoring, bad me for doing two things at once.

--- Assets/Scripts/CS_Enemy_Collision.cs
+++ Assets/Scripts/CS_Enemy_Collision.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,7 +9,6 @@ public class CS_Enemy_Collision : MonoBehaviour {
 
     public bool dead { get; private set; }
     private Rigidbody2D rb;
-    private float timer = 10.0f;
     private float randomSpin;
     public Sprite deathsprite;
     private SpriteRenderer spriteRenderer;
@@ -18,38 +18,55 @@ public class CS_Enemy_Collision : MonoBehaviour {
     private void Start()
     {
         dead = false;
-        randomSpin = Random.Range(-3f, 3f);
+        randomSpin = UnityEngine.Random.Range(-3.0f, 3.0f);
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        GameObject cgo = collision.gameObject;
-        if (cgo.tag == "Projectile")
+        switch (collision.gameObject.tag)
         {
-            CS_Projectile_Collision projectileCollision = cgo.GetComponent<CS_Projectile_Collision>();
-            if (projectileCollision.isAvatar())
-            {
-                if (!CS_WorldManager.Instance.powerupExists && Random.Range(0.0f, 1.0f) <= 0.2f)
-                {
-                    Instantiate(twinShieldPowerUp, transform.position, new Quaternion(), transform.parent);
-                    CS_WorldManager.Instance.powerupExists = true;
-                }
+            case "Projectile":
+                OnProjectileCollisionEnter2D(collision);
+                break;
+            case "Player":
+                OnPlayerCollisionEnter2D(collision);
+                break;
+            case "Shield":
+                OnShieldCollisionEnter2D(collision);
+                break;
+            default:
+                break;
+        }
+    }
 
-                Vector3 dir = collision.contacts[0].point - (Vector2)transform.position;
-                dir = -dir.normalized;
-                OnDeath(dir);
+    private void OnProjectileCollisionEnter2D(Collision2D collision)
+    {
+        CS_Projectile_Collision projectileCollision = collision.gameObject.GetComponent<CS_Projectile_Collision>();
+        if (projectileCollision.isAvatar())
+        {
+            if (!CS_WorldManager.Instance.powerupExists && UnityEngine.Random.Range(0.0f, 1.0f) <= 0.2f)
+            {
+                Instantiate(twinShieldPowerUp, transform.position, new Quaternion(), transform.parent);
+                CS_WorldManager.Instance.powerupExists = true;
             }
+
+            Vector3 dir = collision.contacts[0].point - (Vector2)transform.position;
+            dir = -dir.normalized;
+            OnDeath(dir);
         }
-        else if (cgo.tag == "Shield")
-        {
-            CS_Notifications.Instance.Post(this, "OnAvatarDamage");
-            OnDeath(collision.contacts[0].normal);
-        }
-        else if (cgo.tag == "Player")
-        {
-            OnDeath(collision.contacts[0].normal);
-        }
+    }
+
+    private void OnPlayerCollisionEnter2D(Collision2D collision)
+    {
+        OnDeath(collision.contacts[0].normal);
+    }
+
+    private void OnShieldCollisionEnter2D(Collision2D collision)
+    {
+        CS_Notifications.Instance.Post(this, "OnAvatarDamage");
+        OnDeath(collision.contacts[0].normal);
     }
 
     public void OnDeath(Vector2 direction)
@@ -65,20 +82,19 @@ public class CS_Enemy_Collision : MonoBehaviour {
         spriteRenderer.sprite = deathsprite;
     }
 
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+
+    }
+
     private void Update()
     {
-       
         if (Time.timeScale <= 0) { return; }
 
         if (dead == true)
         {
-            timer -= Time.deltaTime;
             transform.Rotate(Vector3.forward, randomSpin, 0);
-
-            if (timer <= 0)
-            {
-                Destroy(gameObject);
-            }
         }
     }
 }
