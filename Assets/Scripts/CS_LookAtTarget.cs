f9228f03bc825c714fc13343a0f7c3c4e245ccf0
Thomas Cairns <lazycairx@gmail.com>
2017-03-15 10:42:04 +0100

    Look at target script

---
+++ Assets/Scripts/CS_LookAtTarget.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_LookAtTarget : MonoBehaviour
+{
+    public Transform rotateObject;
+    public Transform target;
+
+    void Update()
+    {
+        float angle = CS_Utils.PointToDegree(target.position - rotateObject.position);
+        rotateObject.rotation = Quaternion.Euler(0.0f, 0.0f, angle + 90);
+    }
+}
